2911eb25130d74234eafffa1cb1c7e4d2b8ea21b
Gustavo <gustavo.gonda@gmail.com>
2013-12-15 22:29:34 -0500

    Algunas mejoras en el chat

--- pruebas/chat/site/public/js/script.js
+++ pruebas/chat/site/public/js/script.js
@@ -1,22 +1,100 @@
 	var l=console;
-	var ListMessage = function($scope){
-		$scope.messages = [{
-			message:"hola",
-			remitente:"Gustavo"
-		}]
+	var socket = io.connect("/");
+	var myChatApp = angular.module("myChatApp",[]);
 
-		$scope.sendMessage = function(e){	
-			var tecla = e.keyCode.toString();
-			l.log(tecla)
-			if(tecla == "13"){
-				$scope.messages.push({
-					message:$scope.new_message,
-					remitente:"yo"
-				});
-
-				$scope.new_message = "";
-				//$scope.remitente = "";
-			}	
+	/*controlador Angular*/
+					window.insertarMsj = function(data){
+						window.$scope.messages.push({
+							message:data.msj,
+							remitente:data.remitente,
+							destinatario:data.destinatario
+						});
+					};
+		var btnShowMyId = $(".btnShowMyId");
+	socket.on('connect',function(){
+		window.lanzarMetodo = function(data){
+							socket.emit('window.insertarMsj',data);
+							l.log("emitido desde cliente");
 		}
+		
 
-	}
+		btnShowMyId.on("click",function(e){
+			l.log(window.$scope.remitente);
+			socket.emit("showMyId",{username:window.$scope.remitente});
+		});
+		socket.on('showMyId',function(data){
+			l.log("escuchado desde cliente...btnShowMyId")
+			l.log(data);
+		});
+		socket.on('window.insertarMsj',function(data){
+			l.log("escuchado desde cliente...")
+			l.log(data)
+			window.insertarMsj(data);
+			window.keypear("chat");
+		});
+		socket.on('showUsers',function(data){
+			l.log("nueva lista usuarios: ")
+			l.log(data);
+			window.listUsers = data;
+			window.keypear("listUsers");
+		})
+
+	});
+		myChatApp.controller("ListUsersCtrl",function($scope){
+			$scope.users = [];
+			$scope.add = function(){
+				l.log(window.listUsers.listUsers);
+				var list = window.listUsers.listUsers;
+				for (var i = list.length - 1; i >= 0; i--) {
+					$scope.users.push(list[i]);
+				};
+			}
+		});
+		myChatApp.controller("ListMessageCtrl",function($scope){
+					$scope.remitente	=	location.pathname.substring(location.pathname.lastIndexOf("/")+1);
+					$scope.messages = [{}];
+
+			socket.emit("new_user",{username:$scope.remitente});
+					$scope.sendMessage = function(e){	
+						var tecla = e.keyCode.toString();
+						if(tecla == "13"){
+							window.lanzarMetodo({
+								msj:$scope.new_message,
+								remitente:$scope.remitente,
+								destinatario:null
+							});
+							$scope.new_message = "";
+						}	
+					}//sendMessage
+					window.$scope = $scope;
+		});//ListMessage
+		
+	/*end controlador Angular*/
+		!function($){
+				var input = $('#chat-input');
+				var box = document.querySelector("#messages");
+				var listUsers = $('.listUsers');
+				/*
+				window.clikear = function(){
+					input.trigger("click");
+				}
+				*/
+				window.keypear = function(ctrl){
+					switch(ctrl){
+						case"chat":
+							input.trigger("keypress");
+							/*scrolleando la caja de mensajes*/
+							var dif = (box.scrollHeight - box.scrollTop);
+							box.scrollByLines(dif);
+						break;
+						case"listUsers":
+							listUsers.trigger("keypress",function(){
+								console.log("keypress .....**")
+							});
+						break;
+						default:
+						break;
+					}
+				}
+		}(jQuery)
+
--- pruebas/chat/site/server.js
+++ pruebas/chat/site/server.js
@@ -21,7 +21,7 @@ app.configure(function(){
 	app.use(express.static('./public'));
 	app.use(app.router);
 	app.use(function(req,res){//cada vez que se lanze error de pagina no encontrada 
-		res.send(404,"OOPS!! Page not found");//respondera con este aviso o pagina.
+		res.send(404,"<strong>OOPS!! Page not found</strong>");//respondera con este aviso o pagina.
 	});
 	/*
 	app.use(express.bodyParser());//lanza un aviso de advertencia
@@ -42,9 +42,9 @@ app.get('/',function(req,res){
 });
 
 app.get('/chat/:username',function(req,res){
-	l(__dirname);
+	//l(__dirname);
 	var username = req.params.username;
-	l(username);
+	l(username + " is enter");
 	req.session.username = username;
 	res.render('chat-1.jade',
 		{username:username});
@@ -55,6 +55,71 @@ app.get('/chat',function(req,res){
 		{username:"tavito"});
 });
 
+io.sockets.on('connection',function(socket){
+	l("nueva coneccion...")
+	socket.on("new_user",function(data){
+		socket.set("username",data.username.toString())
+		l("*New user: "+ data.username);
+		socket.emit("showUsers",{
+			listUsers:getListUsersnames(io.sockets.sockets)
+		});
+	});
+	socket.on("window.insertarMsj",function(data){
+		l("escuchado desde servidor...")
+		io.sockets.emit('window.insertarMsj',data);//emitimos el metodo window.insertarMsj
+	});
+
+	socket.on("showMyId",function(data){
+		/*
+		io.sockets.emit("showUsers",
+		{
+			listUsers:getListUsersnames(io.sockets.sockets)
+		});
+		*/
+		var username = data.username.toString();//el username del usuario;
+		var _id = findIdUser(io.sockets.sockets,username);//el _id registrado por socket.io del username del usuario
+		io.sockets.sockets[_id].emit("showUsers",{
+			idUser:findIdUser(io.sockets.sockets,username)
+		});
+
+	});
+	l("clients...");
+	l(io.sockets.clients());
+	l("****sockets...")
+	l(io.sockets.sockets);
+	/*
+	*/
+});
+	var getListUsersnames = function(listSockets){
+		/*
+		*/
+		var listUsers = [];
+		for(var id in listSockets){
+			listSockets[id].get('username',function(err, username){
+			 	listUsers.push(username)
+				});
+		};
+		return listUsers;
+	}
+
+	var findIdUser = function(listSockets,username){
+		/*Este metodo obtiene el _id de un usuario espedifico 
+		conociendo previamente su username
+		
+		*username : El nombre del usuario que se esta buscando
+		*/
+		var _id = null;
+		for(var id in listSockets){
+			l(id);
+			listSockets[id].get("username",function(err,un){
+				l(un)
+				if(username == un)
+					_id = id;
+			});
+		};
+		return _id.toString();
+	}
+		
 
 server.listen(app.get('port'),function(){
 	l("Server runing in port: "+app.get('port'));
