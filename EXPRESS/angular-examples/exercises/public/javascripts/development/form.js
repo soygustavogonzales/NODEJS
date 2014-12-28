a78a934878f73a183717c087c5af0aa315c1701b
gustavo gonzales <gustavo.gonda@gmail.com>
2014-12-28 17:32:53 -0500

    mejorada la directiva dni en EXPRESS/angular../exercises

--- EXPRESS/angular-examples/exercises/public/javascripts/development/form.js
+++ EXPRESS/angular-examples/exercises/public/javascripts/development/form.js
@@ -24,32 +24,45 @@ myapp.directive('dni', ['ftyConnections','$http',function(ftyConnections,$http){
 		restrict:"E"
 		,require: 'ngModel'
 		,compile:function(ele,attrs){
-			console.log(attrs)
+			//console.log(attrs)
 			var input = ele.find('input')
 			var js = JSON.parse(eval(JSON.stringify(attrs.ngg)))
 			for(ngAttr in js){
 					input.attr(ngAttr, js[ngAttr])
 			}
-			myapp["drv-dni"] = attrs
+			myapp["drv-dni"] = new Array()
+			myapp["drv-dni"].push(attrs)
 			for(key in attrs.$attr){
-				if(key==="ngg")
+				if(key==="ngg"||key==="opts")
 					continue
-				console.log(key)
+				//console.log(key)
 				input.attr(key, attrs[key])
 			}
 		}
 		,templateUrl:"/partials/dni-input"
 		,controller:function($scope,$sce){
-			//console.log($sce)
-			console.log($scope.form)
-			console.log(myapp["drv-dni"].required!=undefined)
+			/*
+			*/
+			var lastItem = myapp["drv-dni"][myapp["drv-dni"].length-1]
+			var opts = JSON.parse(eval(JSON.stringify(lastItem.opts)))
+			var form = opts.nameForm
+			//console.log(opts.nameForm)
+			var maxlength = lastItem.maxlength
+			var js = JSON.parse(eval(JSON.stringify(lastItem.ngg)))
+			//console.log(js)
+			var ngModel_ = js["ng-model"]
+			//$scope[form][ngModel_].$setValidity("minlength",false)
+			console.log($scope[form].dni)
 			$scope.clear = function(){
-				if($scope.dni&&myapp["drv-dni"].required!=undefined) {
-					$scope.dni = $scope.dni.replace(/[^0-9]/ig,'');
-					if($scope.dni.length<8)
-						$scope.form.dni.$setValidity("minlength",false)
-					else
-						$scope.form.dni.$setValidity("minlength",true)
+				//console.log($scope[form])
+				console.log($scope[ngModel_])
+				if($scope[ngModel_]&&lastItem.required!=undefined) {
+					$scope[ngModel_] = $scope[ngModel_].replace(/[^0-9]/ig,'');
+					if(lastItem.maxlength)
+						if($scope[ngModel_].length==maxlength)
+							$scope[form][ngModel_].$setValidity("minlength",true)
+						else
+							$scope[form][ngModel_].$setValidity("minlength",false)
 				}
 			}//.clear
 		}
