f1fbe5f23d63daa2e4db239b2a66d0373c770f23
gustavo gonzales <gustavo.gonda@gmail.com>
2015-01-02 23:45:09 -0500

    mejoras en EXPRESS/angular-examples/exercises/public/javascripts/development/form.js

--- EXPRESS/angular-examples/exercises/public/javascripts/development/form.js
+++ EXPRESS/angular-examples/exercises/public/javascripts/development/form.js
@@ -67,11 +67,33 @@ myapp.directive('dni', ['ftyConnections','$http',function(ftyConnections,$http){
 			}//.clear
 		}
 		,link: function($scope, ele, attrs, ngModel) {
-			/*
 			ele.bind('keydown',function(event) {
+				console.log(this)
 			});
+			/*
 			*/
 
+		}
+	};
+}]);
+
+directive('', ['', function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			
 		}
 	};
 }]);
