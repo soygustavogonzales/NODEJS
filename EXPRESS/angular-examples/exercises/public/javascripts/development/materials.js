5010f2c2bb03c036f6ed3e73d3421363b208642d
gustavo gonzales <gustavo.gonda@gmail.com>
2015-03-10 01:10:53 -0500

    agregado ejemplos con materials

--- EXPRESS/angular-examples/exercises/public/javascripts/development/materials.js
+++ EXPRESS/angular-examples/exercises/public/javascripts/development/materials.js
@@ -1,1 +1,7 @@
-angular.module('myapp', ['ngMaterial']);
+var myapp = angular.module('myapp', ['ngMaterial']);
+
+myapp.controller('ctrlData', ['$scope','$mdSidenav', function($scope,$mdSidenav){
+	$scope.openLeft = function(){
+		 $mdSidenav('left').toggle();
+	}
+}])
