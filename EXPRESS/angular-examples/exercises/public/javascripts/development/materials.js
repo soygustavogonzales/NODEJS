a7b87c86602ce8eb2558a16635e43d4ebee4fbd0
gustavo gonzales <gustavo.gonda@gmail.com>
2015-03-13 15:46:27 -0500

    cambios en materials con autocomplete

--- EXPRESS/angular-examples/exercises/public/javascripts/development/materials.js
+++ EXPRESS/angular-examples/exercises/public/javascripts/development/materials.js
@@ -1,5 +1,5 @@
 var myapp = angular.module('myapp', ['ngMaterial']);
-myapp.controller('ctrlData'	, ['$timeout', 'svcPersons', DemoCtrl])
+myapp.controller('ctrlData'	, ['$scope','$timeout', 'svcPersons', DemoCtrl])
 myapp.service('svcPersons',['$q','$http',function($q,$http){
 	this.getPersonsByQuery = function(query){
   	 var config = {
@@ -16,67 +16,32 @@ myapp.service('svcPersons',['$q','$http',function($q,$http){
 	}
 }]);
 
-function DemoCtrl ($timeout, svcPersons) {
+function DemoCtrl ($scope,$timeout,svcPersons) {
+	//console.log($scope)
+	var fiction = false
+		$scope.$watch('ctrl.searchText',function(current,old){
+			if($scope.ctrl.searchText){
+				//console.log($scope.ctrl.searchText)
+				$scope.querySearch()
+			}
+		})
   var self = this;
 
   // list of `state` value/display objects
-  self.states        = loadAll();
   self.selectedItem  = null;
   self.searchText    = null;
-  self.querySearch   = querySearch;
+  //self.querySearch   = querySearch;
   self.simulateQuery = false;
   self.isDisabled    = false;
-
-  function querySearch (query) {
-  	/*
-  	*/
-    var promise = svcPersons.getPersonsByQuery(angular.lowercase(query))
+  $scope.ctrl.states_ = []
+  $scope.querySearch = function(){
+    var promise = svcPersons.getPersonsByQuery(angular.lowercase($scope.ctrl.searchText))
     promise.then(function(response){
-    		console.log(response.data)
-    		return response.data;
+    		$scope.ctrl.states_ = response.data
     }, function(err){
     	console.log(err);
-    });
-
-    var results = query ? self.states.filter( createFilterFor(query) ) : [],deferred;
-    //console.log(self.states);
-    //console.log(self.states.filter( createFilterFor(query)	))
-    console.log(results)
-    		return results;
-
+    })
 
   }
 
-  /**
-   * Build `states` list of key/value pairs
-   */
-  function loadAll() {
-    var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-              Wisconsin, Wyoming';
-
-    return allStates.split(/, +/g).map( function (state) {
-        return {
-          value: state.toLowerCase(),
-          display: state
-        };
-    });
-  }
-
-  /**
-   * Create filter function for a query string
-   */
-  function createFilterFor(query) {
-  		//console.log(query);
-    var lowercaseQuery = angular.lowercase(query);
-    //console.log(lowercaseQuery);
-    return function filterFn(state) {
-      return (state.value.indexOf(lowercaseQuery) === 0);
-    };
-
-  }
 }
