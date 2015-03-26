1936f3f1d50ce46bca44bacf8dc044ad2c620e8f
gustavo gonzales <gustavo.gonda@gmail.com>
2015-03-26 13:31:37 -0500

    agregado ejemplo sufion customElements+shadowDOM

---
+++ EXPRESS/angular-examples/exercises/public/javascripts/development/customElements+shadowDOM.js
@@ -0,0 +1,53 @@
+var myapp = angular.module("myapp",[]);
+myapp.controller('ctrlData', ['$scope',function($scope){
+	$scope.visitor = {
+		name:"xavier",
+		lastname:"charles",
+		age:43,
+		addres:"Av gothika",
+		email:"xcharles@xmen.com"
+	}
+}])
+myapp.directive('drvCard', ['$http','$compile', function($http,$compile){
+	return {
+		scope: {
+			data:"="
+		}, 
+		controller: function($scope, $element, $attrs) {
+			$scope.contact = $scope.data
+		},
+		restrict: 'E',
+		link: function($scope, iElm, iAttrs, controller) {
+			var parent = iElm.parent()
+			//console.log(parent);
+			var htmlDOM = null,$htmlDOM = null;
+			$scope.clean = function(){
+				console.log($scope.name)
+			}
+				$http.get('/partials/drv-card.jade')
+				.then(function(response){
+					htmlDOM = $compile(response.data)($scope)[0]
+					$htmlDOM = $(htmlDOM);
+					$htmlDOM.find('h3').click(function(event) {
+						console.log("click en H3")
+					});
+					//console.log($htmlDOM)
+				}, function(err){console.log(err.data)})
+				.then(function(data){
+						var root = iElm[0].createShadowRoot()
+						$(root).append($htmlDOM)
+						//$('.ocontent').append(iElm)
+
+					var XFoo = document.registerElement('drv-new-card',{
+						  prototype: Object.create(HTMLButtonElement.prototype),
+	  					extends: 'div'
+					});
+					var xfoo = new XFoo()
+					var $xfoo = $(xfoo)
+					$xfoo.append(iElm)
+					//console.log(parent[0].classList)
+					parent.append($xfoo);
+				})
+		}
+	};
+}]);
