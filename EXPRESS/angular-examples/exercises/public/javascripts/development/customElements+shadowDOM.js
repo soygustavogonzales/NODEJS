4e9cb2b1114a432540dc3cd9760966802477bdfb
gustavo gonzales <gustavo.gonda@gmail.com>
2015-03-26 16:29:04 -0500

    improvements in customElement+ShadowDOM in angular examples

--- EXPRESS/angular-examples/exercises/public/javascripts/development/customElements+shadowDOM.js
+++ EXPRESS/angular-examples/exercises/public/javascripts/development/customElements+shadowDOM.js
@@ -8,28 +8,14 @@ myapp.controller('ctrlData', ['$scope',function($scope){
 		email:"xcharles@xmen.com"
 	}
 }])
-myapp.directive('drvCard', ['$http','$compile', function($http,$compile){
-	return {
-		scope: {
-			data:"="
-		}, 
-		controller: function($scope, $element, $attrs) {
-			$scope.contact = $scope.data
-		},
-		restrict: 'E',
-		link: function($scope, iElm, iAttrs, controller) {
-			var htmlDOM = null,$htmlDOM = null, Shadow = null, shadow = null,root = null,son = null;
-			$scope.clean = function(){
-				console.log($scope.name)
-			}
-				$http.get('/partials/drv-card.jade')
+myapp.service('svcShadowDOM',['$http','$q','$compile',function($http,$q,$compile){
+	var htmlDOM = null,$htmlDOM = null, Shadow = null, shadow = null,root = null,son = null;
+	this.getShadowDOM = function(urlDOM,iElm,$scope){
+			var q = $q.defer()
+				$http.get(urlDOM)
 				.then(function(response){
-					htmlDOM = $compile(response.data)($scope)[0]
-					$htmlDOM = $(htmlDOM);
-					$htmlDOM.find('h3').click(function(event) {
-						console.log("click en H3")
-					});
-					//console.log($htmlDOM)
+							htmlDOM = $compile(response.data)($scope)[0]
+							//$htmlDOM = $(htmlDOM);
 				}, function(err){console.log(err.data)})
 				.then(function(data){
 								Shadow = document.registerElement('drv-card',{
@@ -39,10 +25,34 @@ myapp.directive('drvCard', ['$http','$compile', function($http,$compile){
 								shadow = new Shadow()
 								iElm.append(shadow)
 								root = shadow.createShadowRoot()
-								$(root).append($htmlDOM)
+								$(root).append(htmlDOM)
 								son = $(iElm.children('[is]'));
 								iElm.replaceWith(son)
-								})
+								q.resolve(htmlDOM)
+					})
+
+			return q.promise;
+	}
+}]);
+myapp.directive('drvCard', ['$http','$compile','svcShadowDOM', function($http,$compile,svcShadowDOM){
+	return {
+		scope: {
+			data:"="
+		}, 
+		controller: function($scope, $element, $attrs) {
+			$scope.contact = $scope.data
+		},
+		restrict: 'E',
+		link: function($scope, iElm, iAttrs, controller) {
+				svcShadowDOM.getShadowDOM('/partials/drv-card.jade',iElm,$scope)
+				.then(function(html){
+						
+						$(html).find('figure').hover(function() {
+								console.log('on')
+						}, function() {
+								console.log('out')
+						});
+				})
 		}
 	};
 }]);
