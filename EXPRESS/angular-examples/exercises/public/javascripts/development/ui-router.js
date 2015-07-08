1670c9d1ff774438972bf4778197e00740ecc082
gustavo gonzales <gustavo.gonda@gmail.com>
2015-07-08 06:43:55 +0100

    agregado ejmplo con ui-router

---
+++ EXPRESS/angular-examples/exercises/public/javascripts/development/ui-router.js
@@ -0,0 +1,35 @@
+var myapp = angular.module("myapp",['ui.router']);
+myapp.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+		$stateProvider
+		.state('home',{
+			url:'/',
+			templateUrl:'/partials_/home.html',
+			controller:'ctrlHome'
+		})
+		.state('about',{
+			url:'/about',
+			templateUrl:'/partials_/about.html',
+			controller:'ctrlAbout'
+		})
+		.state('users',{
+			url:'/users/:userId',
+			templateUrl:'/partials_/users.html',
+			controller:'ctrlUsers'
+		})
+}])
+
+myapp.controller('ctrlAbout', ['$scope','$log', function($scope,$log){
+	console.log(">>> ctrlAbout")
+}])
+
+myapp.controller('ctrlHome', ['$scope','$log', function($scope,$log){
+	console.log(">>> ctrlHome")
+	
+}])
+
+myapp.controller('ctrlUsers', ['$scope','$stateParams', function($scope,$stateParams){
+	console.log($scope)
+	console.log($stateParams.userId)
+	
+}])
